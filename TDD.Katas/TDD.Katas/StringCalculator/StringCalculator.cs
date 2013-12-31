87f40b29b8924a9bb9cf3a3eaad9cd8da386ecc1
Konrad Korus <konradkorus@gmail.com>
2013-12-31 15:39:45 +0100

    changes

---
+++ TDD.Katas/TDD.Katas/StringCalculator/StringCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Katas.StringCalculator
+{
+    /// <summary>
+    /// http://osherove.com/tdd-kata-1/
+    /// </summary>
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            return int.Parse(numbers);
+        }
+    }
+}
---
+++ TDD.Katas/TDD.Katas/StringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDD.Katas.StringCalculator
+{
+    public class StringCalculatorTest
+    {
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            var sc = CreateCalculator();
+
+            int result = sc.Add("");
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void Add_SingleNumber_ReturnsThatNumber(string numbers, int exptected)
+        {
+            var sc = CreateCalculator();
+
+            var result = sc.Add(numbers);
+
+            Assert.AreEqual(exptected, result);
+        }
+
+        [Test]
+        public void Add_MultipleNumbers_SumsThemUp()
+        {
+            var sc = CreateCalculator();
+
+            int result = sc.Add("1,2");
+            
+            Assert.AreEqual(3, result);
+
+
+        }
+        private static StringCalculator CreateCalculator()
+        {
+            return new StringCalculator();
+        }
+    }
+}
